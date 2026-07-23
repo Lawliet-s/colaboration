0d4a02f5c9a50b9cfafa172ae33375dab54f35be
Lawliet-s <rifqicoding@gmail.com>
2026-07-23 09:48:38 +0700

    edit script.js and index.html

---
+++ feature 1/script.js
@@ -0,0 +1,8 @@
+document.getElementById("toggle").addEventListener("click", function() {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+    });
+    document.getElementById("toggle2").addEventListener("click", function() {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+    });
